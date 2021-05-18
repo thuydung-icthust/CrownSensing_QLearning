86b9706dd2042e0d2d3b833a98f4fcc1cbf7bf67
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2021-05-18 09:01:40 +0700

    finish traning single agent

--- QLearning/config.py
+++ QLearning/config.py
@@ -1,10 +1,10 @@
 USE_GPU = False
-N_EPISODE = 10000  # The number of episodes for training
+N_EPISODE = 200  # The number of episodes for training
 MAX_STEP = 1000  # The number of steps for each episode
 BATCH_SIZE = 32  # The number of experiences for each replay
 MEMORY_SIZE = 100000  # The size of the batch for storing experiences
 # After this number of episodes, the DQN model is saved for testing later.
-SAVE_NETWORK = 50
+SAVE_NETWORK = 10
 # The number of experiences are stored in the memory batch before starting replaying
 INITIAL_REPLAY_SIZE = 100
 INPUTNUM = 10000  # The number of input values for the DQN model
